4f540a1d980038e2cc469bdb85929d658e3207c2
Jared Hanson <jaredhanson@gmail.com>
2023-02-27 13:09:01 -0800

    Remove TODO item.

--- com/user/serialize.js
+++ com/user/serialize.js
@@ -1,7 +1,5 @@
 exports = module.exports = function() {
   return function(user, cb) {
-    // TODO: only do displayName, image, and username
-    
     process.nextTick(function() {
       var obj = { id: user.id };
       if (user.username) { obj.username = user.username; }
