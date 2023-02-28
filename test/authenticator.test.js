4a1392eb9e077ab94c5c5e693f8c1abd3e42d857
Jared Hanson <jaredhanson@gmail.com>
2023-02-28 09:59:36 -0800

    Add tests.

--- test/authenticator.test.js
+++ test/authenticator.test.js
@@ -1,18 +1,30 @@
-/* global describe, it, expect */
-
-var factory = require('../com/authenticator');
 var expect = require('chai').expect;
+var $require = require('proxyquire');
+var sinon = require('sinon');
+var factory = require('../com/authenticator');
+var Authenticator = require('passport').Authenticator;
 
 
 describe('authenticator', function() {
   
-  it('should export factory function', function() {
-    expect(factory).to.be.a('function');
+  it('should be annotated', function() {
+    expect(factory['@singleton']).to.equal(true);
+    expect(factory['@implements']).to.equal('module:@authnomicon/session.Authenticator');
   });
   
-  it('should be annotated', function() {
-    expect(factory['@implements']).to.equal('module:@authnomicon/session.Authenticator');
-    expect(factory['@singleton']).to.equal(true);
+  it('should construct Authenticator', function() {
+    var AuthenticatorSpy = sinon.spy(Authenticator);
+    var factory = $require('../com/authenticator', {
+      'passport': { Authenticator: AuthenticatorSpy }
+    });
+    
+    var scheme = new Object();
+    var manager = new Object();
+    var authenticator = factory(scheme, manager);
+    
+    expect(AuthenticatorSpy).to.have.been.calledOnce;
+    expect(AuthenticatorSpy).to.have.been.calledWithNew;
+    // TODO: Assertions for using scheme and manager
   });
   
 });
