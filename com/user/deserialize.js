68a4b69c79822f0c14c61e3b5953286eb0a5be28
Jared Hanson <jaredhanson@gmail.com>
2023-06-01 06:33:51 -0700

    Add JSdoc.

--- com/user/deserialize.js
+++ com/user/deserialize.js
@@ -1,3 +1,15 @@
+/**
+ * Create user deserialization function.
+ *
+ * Returns a function that deserializes a user object from data stored in a
+ * session to an object expected by the application.
+ *
+ * The data stored in the session was previously serialized by the corresponding
+ * `serialize` function.  It is deserialized into an object that conforms to
+ * Passport's {@link https://www.passportjs.org/reference/normalized-profile/ normalized profile}.
+ *
+ * @returns {passport.Authenticator~deserializeUserFn}
+ */
 exports = module.exports = function() {
   return function(obj, cb) {
     process.nextTick(function() {
